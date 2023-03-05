f160288e976b630878f6ab126d17fe5f2bcf51e6
TANISHA-P <81011955+TANISHA-P@users.noreply.github.com>
2023-03-05 17:11:36 +0530

    Delete New directory

--- New/projects/portfolio/Portfolio-master/portfolio/projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.project,name = "project"),
-    path('articles', views.article,name = "article"),
-    path('paintings',views.paintings,name = "painting")
-]
