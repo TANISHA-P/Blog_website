5393bfda0af61135fcfe11560f3fa5cc9a21397b
TANISHA-P <tanishapareek2002@gmail.com>
2023-04-22 13:41:38 +0530

    Final changes done in settings.py

--- Writers_pursuit/Writers_pursuit/settings.py
+++ Writers_pursuit/Writers_pursuit/settings.py
@@ -25,9 +25,10 @@ print(BASE_DIR)
 SECRET_KEY = 'django-insecure-7lm6^19f=)qf975m*mjrp)&jw@1w^bijtov#bgl@%w8hv58t@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['writers-pursuit.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
