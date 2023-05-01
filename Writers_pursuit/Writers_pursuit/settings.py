00b0d60dc503e7ca6b66852a14b6d2bead9b150b
TANISHA-P <tanishapareek2002@gmail.com>
2023-05-01 22:48:15 +0530

    removing whitenoise

--- Writers_pursuit/Writers_pursuit/settings.py
+++ Writers_pursuit/Writers_pursuit/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
